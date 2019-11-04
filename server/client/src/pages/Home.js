557c0c283217a3b6c9084f282af77f31fde8c1b1
gundrm <rachelm.gundy@gmail.com>
2019-11-03 20:13:42 -0800

    tournament and matches page UI update

--- server/client/src/pages/Home.js
+++ server/client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Tournaments from '../components/Tournaments';
+import Image from '../assets/helmet.jpeg';
 
 
 
@@ -9,6 +10,12 @@ function Home() {
     return(
         <div className="mt-5">
             <Container fluid={true}>
+                    <img 
+                        className="img-tourn" 
+                        src={Image} 
+                        alt="Logo" 
+                        style={{disply: "flex",
+                                width: "100%"}} />
                 <Tournaments />
             </Container>
         </div>
