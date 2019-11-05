35dd51cf39bfb5dfc7a1ed6f6f8647fa1812465c
adrianctwo <adrian.e.situ@gmail.com>
2019-11-04 18:03:42 -0800

    updates

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,6 +2,7 @@ module.exports = {
     googleClientID: '840149302610-jphsdcg4o2ojjtc62un63tp1m0j2hibi.apps.googleusercontent.com',
     googleClientSecret: 'Yrj3zA7UPMg2h6RjdGwwtQYo',
     mongoURI: 'mongodb+srv://rachel:rachel123@cluster0-s02ox.mongodb.net/league-db?retryWrites=true&w=majority',
-    cookieKey: 'oihasdflkerpoiuasdfhoiuh'
+    cookieKey: 'oihasdflkerpoiuasdfhoiuh',
+    pandaKey: 'X3drf1j8uCkYcZ1s1U0lpT_TXuzRqLqlCad7B08cGTsKSSm2moU'
 };
 
